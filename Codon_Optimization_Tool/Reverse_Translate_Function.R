5394169310137c5802bb7d8d8e8c662f7241da50
anchitaa <anchitaaghag@gmail.com>
2022-07-12 01:54:11 -0400

    Edited the Reverse_Translate() function to take two user inputs instead of three. In addition, I have added more commenting throughout the file.

--- Codon_Optimization_Tool/Reverse_Translate_Function.R
+++ Codon_Optimization_Tool/Reverse_Translate_Function.R
@@ -1,14 +1,10 @@
 # Reverse_Translate Function
 # Anchitaa Ghag
 
-# This function requires three inputs. The name of a file containing the amino acid sequences, a name for the results file, and a logical TRUE/FALSE input for the type of codon "usage" to use.
+# This function requires two inputs. The name of a file containing the amino acid sequences and a logical TRUE/FALSE input for the type of codon "usage" to use.
 # The codon usage (i.e. proportions) will be calculated by the function below. However, the user will need to provide a list of empirical codon counts or frequencies per codon.
 
-sequence_file <- "Example_Protein_Sequences.txt"
-output_file_name <- "Example_Results.txt"
-default_codon_usage <- TRUE
-
-#Reverse_Translate <- function(sequence_file, output_file_name, default_codon_usage) {
+Reverse_Translate <- function(sequence_file, default_codon_usage) {
   
   # Read in the protein sequence file.
 
@@ -20,35 +16,43 @@ default_codon_usage <- TRUE
   
   # Recursively keep only even numbered lines i.e., the amino acid sequences
   
-  AmAcidSeqs <- SeqFile[c(FALSE, TRUE)] 
+  All_Sequences <- SeqFile[c(FALSE, TRUE)] 
+  
+  rm(SeqFile)
   
   # Read in the default codon usage table or provide an option for the user to enter the name of a file with a custom codon usage table.
   
   if (default_codon_usage == TRUE) { 
-    Codon_Usage_Table <- read.table("Default_Codon_Counts.txt")
+    
+    Codon_Usage_Table <- read.table("Default_Codon_Counts.txt",
+                                    header = TRUE)
+    
   } else if  (default_codon_usage == FALSE) {
+    
     Custom_Codon_Usage_File <- readline(prompt="Please enter the name of the file with your custom counts per codon information: ") # Adapted from: https://stackoverflow.com/questions/60090558/r-language-user-input-if-condition
-    Codon_Usage_Table <- read.table(Custom_Codon_Usage_File)
+    Codon_Usage_Table <- read.table(Custom_Codon_Usage_File,
+                                    header = TRUE)
+    
   } else {
+    
     print("A codon usage was not indicated. The default codon usage of Nicotiana benthamiana will be used.")
-    Codon_Usage_Table <-  read.table("Default_Codon_Counts.txt")
+    Codon_Usage_Table <-  read.table("Default_Codon_Counts.txt",
+                                     header = TRUE)
   }
   
   # Create an Amino_Acid_Lookup list that contains the codons and proportions per amino acid. This will be done using the codon usage from above.
+  # Ensure that the Codon_Usage_Table is listed alphabetically be codon.
   
-  order()
+  dfAmAcid_Prob <- Codon_Usage_Table[order(Codon_Usage_Table$Codons),]
+  rm(Codon_Usage_Table)
   
-  # Assign the single letter abbreviation for all amino acids. This is assuming that codons are listed in an alphabetical order ranging from AAA to TTT (as done in the previous step).
+  # Assign the single letter abbreviation for all amino acids. This is assuming that codons are listed in an alphabetical order ranging from AAA to TTT (as done in the previous step). In addition, "X"s are assigned for the three stop codons
   
-  Single_Letter_Abbreviation <- c("K","N","K","N","T","T","T","T","R","S","R","S","I","I","M","I","Q","H","Q","H","P","P","P","P","R","R","R","R","L","L","L","L","E","D","E","D","A","A","A","A","G","G","G","G","V","V","V","V","X","Y","X","Y","S","S","S","S","X","C","W","C","L","F","L","F")
-  
-  dfAmAcid_Prob <- data.frame(dfNew_MeanSD$`Sum of Counts Per Codon`,rownames(dfNew_MeanSD), Single_Letter_Abbreviation)
-  colnames(dfAmAcid_Prob) <- c("Counts_Per_Codon", "Codons", "Single_Letter_Abbreviation")
-  
-  # "X" for the three stop codons
+  dfAmAcid_Prob["Single_Letter_Abbreviation"] <- c("K","N","K","N","T","T","T","T","R","S","R","S","I","I","M","I","Q","H","Q","H","P","P","P","P","R","R","R","R","L","L","L","L","E","D","E","D","A","A","A","A","G","G","G","G","V","V","V","V","X","Y","X","Y","S","S","S","S","X","C","W","C","L","F","L","F")
   
   Amino_Acids <- list("A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","X","Y")
   
+  # Create an empty list to hold the proportions of codons per amino acid. (i.e., the codon usage)
   
   Amino_Acid_Lookup <- list()
   
@@ -60,13 +64,13 @@ default_codon_usage <- TRUE
     
     Total <- dfAmAcid_Prob %>% 
       filter(Single_Letter_Abbreviation == i) %>%
-      select(Counts_Per_Codon) %>%
+      select(Counts) %>%
       colSums() 
     
     Prop <- dfAmAcid_Prob %>% 
       filter(Single_Letter_Abbreviation == i) %>%
-      select(Counts_Per_Codon) %>%
-      summarise(Proportions = (Counts_Per_Codon/Total)*1) 
+      select(Counts) %>%
+      summarise(Proportions = (Counts/Total)*1) 
     
     Info <- c(Prop,Codon_Names)
     
@@ -74,12 +78,17 @@ default_codon_usage <- TRUE
     
   }
   
-  names(Amino_Acid_Lookup) <- c("A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","X","Y")
-
+  # Rename the lists to match the single letter abbreviations of the amino acids.
   
-  All_Sequences <- as.list(dfNames_Sequences$Sequences)
+  names(Amino_Acid_Lookup) <- unlist(Amino_Acids)
   
-  # Random replacement of values.
+  rm(dfAmAcid_Prob,Codon_Names, Total, Prop, Info, Amino_Acids)
+  
+  # Create an empty list to store all the correspoinding DNA sequences.
+  
+  All_DNA_Seqs <- list()
+  
+  # Next, reiterate through each sequence in the All_Sequences list and randomly replace each amino acid with a corresponding triplet/codon based on the proportions in the Amino_Acid_Lookup.
   
   for (One_Sequence in All_Sequences) {
     
@@ -277,7 +286,7 @@ default_codon_usage <- TRUE
       }
     }
     
-    # Next, add a stop codon.
+    # Next, add a stop codon to the end of the DNA sequence using the Amino_Acid_Lookup list for "X" or stop codons.
     
     Triplet <- sample(x = Amino_Acid_Lookup$X$Codons,
                       size = 1,
@@ -286,21 +295,21 @@ default_codon_usage <- TRUE
     
     DNA_Sequence <- append(DNA_Sequence, Triplet)
     
+    # Then, collapse the DNA_Sequence "list of lists" into one long string.
     
+    Formatted_DNA_Seq <- (paste((unlist(DNA_Sequence)), collapse = ""))
     
-    print(paste((unlist(DNA_Sequence)), collapse = ""))
+    # Finally, append this sequence to the list of All_DNA_Seqs.
     
-    print(c("Your results have been written to:", Output_File_Name))
-    
-    Names
-    output_file_name
-    
-    writeLines()
-    
-    
-    return()
-    
-
+    All_DNA_Seqs <- append(All_DNA_Seqs,Formatted_DNA_Seq)
     
   }
+  
+  # Output the results of alternating protein names in the Names list and the DNA sequences in the All_DNA_Seqs list.
+  
+  Results <- unlist(c(rbind(Names, All_DNA_Seqs)))
+  
+  return(Results)
+  
 }
+

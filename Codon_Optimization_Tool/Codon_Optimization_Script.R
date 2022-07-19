afb63638b8863e1dac91032ea22615859347bc47
anchitaa <anchitaaghag@gmail.com>
2022-07-18 21:53:55 -0400

    Added a new comment on line 5, indicating that the necessary packages must be downloaded prior to running the script for the first time.

--- Codon_Optimization_Tool/Codon_Optimization_Script.R
+++ Codon_Optimization_Tool/Codon_Optimization_Script.R
@@ -1,7 +1,8 @@
 # Codon Optimization Script
 # Anchitaa Ghag
 
-# This R code will require three user inputs in section 01 and will then run the Reverse_Translate() function to optimize codons in a chosen organism. 
+# This R code will require three user inputs in section 01 and will then run the Reverse_Translate() function to optimize codons in a chosen organism.
+# If running this script for the first time, ensure that the two neccessary packages have been installed by uncommenting (i.e. removing the "#") and running lines 33 & 34.
 
 #### 01 USER INPUT ####
 
